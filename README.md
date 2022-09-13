820d3ba0c8c32c8f700431d689c44c2eaff29cd1
Katharina Bäuml <katharina.baeuml@immowelt.de>
2022-09-13 09:51:58 +0200

    inital commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # first_Website
+## Taks 
+ | Addition | substratkion | mulitplikation |  diviison  und dann  -> rechnenregeln 
+ ableiten; Inegrieren; ggt; kgt; ploten 
--- forsite.js
+++ forsite.js
@@ -1,1 +1,20 @@
-console.log("Hello World ");
+
+const calculation = "5+8+12*5";
+const calculationArray = calculation.replaceAll("+", " + ").replaceAll("*", " * ").split(" ");
+
+console.log(calculationArray)
+
+for (let counter = 0; counter < calculationArray.length; counter++) {
+    const element =calculationArray[counter];
+    console.log(element)
+if (element==="*") {
+    console.log(calculationArray[counter-1]*calculationArray[counter+1])
+    const ergebnis = calculationArray[counter-1]*calculationArray[counter+1]
+    calculationArray[counter-1]= ergebnis
+    calculationArray[counter+1]= ""
+    calculationArray[counter]= ""
+   
+    calculationArray=calculationArray.filter(e => e) 
+    console.log(calculationArray)
+}    
+}
