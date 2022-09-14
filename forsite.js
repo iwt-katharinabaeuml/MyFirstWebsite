41538473f1ecdabc0f3ad02694364b5f49d05c1a
Katharina Bäuml <katharina.baeuml@immowelt.de>
2022-09-14 12:40:15 +0200

    Add/sub/multi/div works nice

--- forsite.js
+++ forsite.js
@@ -1,14 +1,21 @@
-const calculation = "12*4/2*2*3";
-const calculationArray = calculation
-  .replaceAll("+", " + ")
-  .replaceAll("*", " * ")
-  .replaceAll("/", " / ")
-  .replaceAll("-", " - ")
-  .split(" ");
+const calculation = "3+11*9/-2+46.5";
+
+
+
+let newstring = calculation.split(/(?<=[0-9])(\+|\-|\*|\/)(?=[0-9])|(?<=[0-9])(\+|\-|\*|\/)(?=\-[0-9])/);
+newstring = newstring.filter(a => a);
+let test = [];
+
+console.log(newstring);
+
+
+const calculationArray = newstring
+
 
 console.log(calculationArray);
 
-while(calculationArray.includes("/") || calculationArray.includes("*")){
+while (calculationArray.includes("/") || calculationArray.includes("*")) {
+    let counter;
   for (let counter = 0; counter < calculationArray.length; counter++) {
     const element = calculationArray[counter];
     console.log(element);
@@ -25,6 +32,7 @@ while(calculationArray.includes("/") || calculationArray.includes("*")){
       calculationArray[counter] = "y";
       calculationArray.splice(calculationArray.indexOf("x"), 1);
       calculationArray.splice(calculationArray.indexOf("y"), 1);
+      counter = 0
       console.log(calculationArray);
     }
     if (element === "/") {
@@ -40,12 +48,13 @@ while(calculationArray.includes("/") || calculationArray.includes("*")){
       calculationArray[counter] = "y";
       calculationArray.splice(calculationArray.indexOf("x"), 1);
       calculationArray.splice(calculationArray.indexOf("y"), 1);
+      counter = 0
       console.log(calculationArray);
     }
   }
 }
 
-while(calculationArray.includes("+") || calculationArray.includes("-")){
+while (calculationArray.includes("+") || calculationArray.includes("-")) {
   for (let counter = 0; counter < calculationArray.length; counter++) {
     const element = calculationArray[counter];
     console.log(element);
@@ -62,6 +71,7 @@ while(calculationArray.includes("+") || calculationArray.includes("-")){
       calculationArray[counter] = "y";
       calculationArray.splice(calculationArray.indexOf("x"), 1);
       calculationArray.splice(calculationArray.indexOf("y"), 1);
+      counter = 0
       console.log(calculationArray);
     }
     if (element === "-") {
@@ -77,6 +87,7 @@ while(calculationArray.includes("+") || calculationArray.includes("-")){
       calculationArray[counter] = "y";
       calculationArray.splice(calculationArray.indexOf("x"), 1);
       calculationArray.splice(calculationArray.indexOf("y"), 1);
+      counter = 0
       console.log(calculationArray);
     }
   }
