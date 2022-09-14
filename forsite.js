8c3d35dc81ba185d60e7d2850bffeb091b356f7f
Katharina Bäuml <katharina.baeuml@immowelt.de>
2022-09-14 10:23:47 +0200

    add Numbers()

--- forsite.js
+++ forsite.js
@@ -1,83 +1,91 @@
+const calculation = "12+12-12-2";
+const calculationArray = calculation
+  .replaceAll("+", " + ")
+  .replaceAll("*", " * ")
+  .replaceAll("/", " / ")
+  .replaceAll("-", " - ")
+  .split(" ");
 
-const calculation = "2*3/2";
-const calculationArray = calculation.replaceAll("+", " + ").replaceAll("*", " * ").replaceAll("/", " / ").replaceAll("-", " - ").split(" ");
+console.log(calculationArray);
 
-console.log(calculationArray)
-
-while (calculationArray.includes ("*")){
-for (let counter = 0; counter < calculationArray.length; counter++) {
-    const element =calculationArray[counter];
-    console.log(element)
-if (element==="*") {
-    console.log(calculationArray[counter-1]*calculationArray[counter+1])
-    const ergebnis = calculationArray[counter-1]*calculationArray[counter+1]
-    calculationArray[counter-1]= ergebnis
-    calculationArray[counter+1]= "x"
-    calculationArray[counter]= "y"
-    calculationArray.splice (calculationArray.indexOf('x'),1);
-    calculationArray.splice (calculationArray.indexOf('y'),1);
-    console.log(calculationArray)
-}    
-}
-}
-
-
-while (calculationArray.includes ("/")){
-    for (let counter = 0; counter < calculationArray.length; counter++) {
-        const element =calculationArray[counter];
-        console.log(element)
-    if (element==="/") {
-        console.log(calculationArray[counter-1]/calculationArray[counter+1])
-        const ergebnis = calculationArray[counter-1]/calculationArray[counter+1]
-        calculationArray[counter-1]= ergebnis
-        calculationArray[counter+1]= "x"
-        calculationArray[counter]= "y"
-        calculationArray.splice (calculationArray.indexOf('x'),1);
-        calculationArray.splice (calculationArray.indexOf('y'),1);
-        console.log(calculationArray)
-    }    
+while (calculationArray.includes("*")) {
+  for (let counter = 0; counter < calculationArray.length; counter++) {
+    const element = calculationArray[counter];
+    console.log(element);
+    if (element === "*") {
+      console.log(
+        Number(calculationArray[counter - 1]) * Number(calculationArray[counter + 1])
+      );
+      const ergebnis =
+        Number(calculationArray[counter - 1]) * Number(calculationArray[counter + 1]);
+      calculationArray[counter - 1] = ergebnis;
+      calculationArray[counter + 1] = "x";
+      calculationArray[counter] = "y";
+      calculationArray.splice(calculationArray.indexOf("x"), 1);
+      calculationArray.splice(calculationArray.indexOf("y"), 1);
+      console.log(calculationArray);
     }
-    }
-
-while (calculationArray.includes ("+")){
-for (let counter = 0; counter < calculationArray.length; counter++) {
-  const element = calculationArray[counter]
-  console.log(element)
-if (element ==="+"){
-    console.log(calculationArray[counter-1]+calculationArray[counter+1])
-    const ergebnis = Number(calculationArray[counter-1])+Number(calculationArray[counter+1])
-    calculationArray[counter-1]= ergebnis
-    calculationArray[counter+1]= "x"
-    calculationArray[counter]= "y"
-    calculationArray.splice (calculationArray.indexOf('x'),1);
-    calculationArray.splice (calculationArray.indexOf('y'),1);
-    console.log (calculationArray)
-}
-}
+  }
 }
 
-while (calculationArray.includes ("-")){
-    for (let counter = 0; counter < calculationArray.length; counter++) {
-      const element = calculationArray[counter]
-      console.log(element)
-    if (element ==="-"){
-        console.log(calculationArray[counter-1]-calculationArray[counter+1])
-        const ergebnis = Number(calculationArray[counter-1])-Number(calculationArray[counter+1])
-        calculationArray[counter-1]= ergebnis
-        calculationArray[counter+1]= "x"
-        calculationArray[counter]= "y"
-        calculationArray.splice (calculationArray.indexOf('x'),1);
-        calculationArray.splice (calculationArray.indexOf('y'),1);
-        console.log (calculationArray)
+while (calculationArray.includes("/")) {
+  for (let counter = 0; counter < calculationArray.length; counter++) {
+    const element = calculationArray[counter];
+    console.log(element);
+    if (element === "/") {
+      console.log(
+        Number(calculationArray[counter - 1]) / Number(calculationArray[counter + 1])
+      );
+      const ergebnis =
+        Number(calculationArray[counter - 1]) / Number(calculationArray[counter + 1]);
+      calculationArray[counter - 1] = ergebnis;
+      calculationArray[counter + 1] = "x";
+      calculationArray[counter] = "y";
+      calculationArray.splice(calculationArray.indexOf("x"), 1);
+      calculationArray.splice(calculationArray.indexOf("y"), 1);
+      console.log(calculationArray);
     }
+  }
+}
+
+while (calculationArray.includes("+")) {
+  for (let counter = 0; counter < calculationArray.length; counter++) {
+    const element = calculationArray[counter];
+    console.log(element);
+    if (element === "+") {
+      console.log(
+        Number(calculationArray[counter - 1]) + Number(calculationArray[counter + 1])
+      );
+      const ergebnis =
+        Number(calculationArray[counter - 1]) +
+        Number(calculationArray[counter + 1]);
+      calculationArray[counter - 1] = ergebnis;
+      calculationArray[counter + 1] = "x";
+      calculationArray[counter] = "y";
+      calculationArray.splice(calculationArray.indexOf("x"), 1);
+      calculationArray.splice(calculationArray.indexOf("y"), 1);
+      console.log(calculationArray);
     }
+  }
+}
+
+while (calculationArray.includes("-")) {
+  for (let counter = 0; counter < calculationArray.length; counter++) {
+    const element = calculationArray[counter];
+    console.log(element);
+    if (element === "-") {
+      console.log(
+        Number(calculationArray[counter - 1]) - Number(calculationArray[counter + 1])
+      );
+      const ergebnis =
+        Number(calculationArray[counter - 1]) -
+        Number(calculationArray[counter + 1]);
+      calculationArray[counter - 1] = ergebnis;
+      calculationArray[counter + 1] = "x";
+      calculationArray[counter] = "y";
+      calculationArray.splice(calculationArray.indexOf("x"), 1);
+      calculationArray.splice(calculationArray.indexOf("y"), 1);
+      console.log(calculationArray);
     }
-
-
-
-let Endergebnis = 0
-for (let counter = 0; counter < calculationArray.length; counter++) {
-   
-    Endergebnis += Number(calculationArray[counter]);
-    console.log(Endergebnis)
+  }
 }
