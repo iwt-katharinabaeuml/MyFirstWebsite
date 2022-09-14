83abcde58254499489005c0938b4e4830cb1dee1
Katharina Bäuml <katharina.baeuml@immowelt.de>
2022-09-14 10:49:40 +0200

    all in 2 whiles

--- forsite.js
+++ forsite.js
@@ -1,4 +1,4 @@
-const calculation = "12+12-12-2";
+const calculation = "12*4/2*2*3";
 const calculationArray = calculation
   .replaceAll("+", " + ")
   .replaceAll("*", " * ")
@@ -8,16 +8,18 @@ const calculationArray = calculation
 
 console.log(calculationArray);
 
-while (calculationArray.includes("*")) {
+while(calculationArray.includes("/") || calculationArray.includes("*")){
   for (let counter = 0; counter < calculationArray.length; counter++) {
     const element = calculationArray[counter];
     console.log(element);
     if (element === "*") {
       console.log(
-        Number(calculationArray[counter - 1]) * Number(calculationArray[counter + 1])
+        Number(calculationArray[counter - 1]) *
+          Number(calculationArray[counter + 1])
       );
       const ergebnis =
-        Number(calculationArray[counter - 1]) * Number(calculationArray[counter + 1]);
+        Number(calculationArray[counter - 1]) *
+        Number(calculationArray[counter + 1]);
       calculationArray[counter - 1] = ergebnis;
       calculationArray[counter + 1] = "x";
       calculationArray[counter] = "y";
@@ -25,39 +27,13 @@ while (calculationArray.includes("*")) {
       calculationArray.splice(calculationArray.indexOf("y"), 1);
       console.log(calculationArray);
     }
-  }
-}
-
-while (calculationArray.includes("/")) {
-  for (let counter = 0; counter < calculationArray.length; counter++) {
-    const element = calculationArray[counter];
-    console.log(element);
     if (element === "/") {
       console.log(
-        Number(calculationArray[counter - 1]) / Number(calculationArray[counter + 1])
+        Number(calculationArray[counter - 1]) /
+          Number(calculationArray[counter + 1])
       );
       const ergebnis =
-        Number(calculationArray[counter - 1]) / Number(calculationArray[counter + 1]);
-      calculationArray[counter - 1] = ergebnis;
-      calculationArray[counter + 1] = "x";
-      calculationArray[counter] = "y";
-      calculationArray.splice(calculationArray.indexOf("x"), 1);
-      calculationArray.splice(calculationArray.indexOf("y"), 1);
-      console.log(calculationArray);
-    }
-  }
-}
-
-while (calculationArray.includes("+")) {
-  for (let counter = 0; counter < calculationArray.length; counter++) {
-    const element = calculationArray[counter];
-    console.log(element);
-    if (element === "+") {
-      console.log(
-        Number(calculationArray[counter - 1]) + Number(calculationArray[counter + 1])
-      );
-      const ergebnis =
-        Number(calculationArray[counter - 1]) +
+        Number(calculationArray[counter - 1]) /
         Number(calculationArray[counter + 1]);
       calculationArray[counter - 1] = ergebnis;
       calculationArray[counter + 1] = "x";
@@ -69,13 +45,29 @@ while (calculationArray.includes("+")) {
   }
 }
 
-while (calculationArray.includes("-")) {
+while(calculationArray.includes("+") || calculationArray.includes("-")){
   for (let counter = 0; counter < calculationArray.length; counter++) {
     const element = calculationArray[counter];
     console.log(element);
+    if (element === "+") {
+      console.log(
+        Number(calculationArray[counter - 1]) +
+          Number(calculationArray[counter + 1])
+      );
+      const ergebnis =
+        Number(calculationArray[counter - 1]) +
+        Number(calculationArray[counter + 1]);
+      calculationArray[counter - 1] = ergebnis;
+      calculationArray[counter + 1] = "x";
+      calculationArray[counter] = "y";
+      calculationArray.splice(calculationArray.indexOf("x"), 1);
+      calculationArray.splice(calculationArray.indexOf("y"), 1);
+      console.log(calculationArray);
+    }
     if (element === "-") {
       console.log(
-        Number(calculationArray[counter - 1]) - Number(calculationArray[counter + 1])
+        Number(calculationArray[counter - 1]) -
+          Number(calculationArray[counter + 1])
       );
       const ergebnis =
         Number(calculationArray[counter - 1]) -
