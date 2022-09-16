ff60cb0d49f52ff44d3faecf75856b2e11d82bc7
Katharina Bäuml <katharina.baeuml@immowelt.de>
2022-09-16 11:46:55 +0200

    runs incl. √, dank Basti

--- forsite.js
+++ forsite.js
@@ -1,119 +1,96 @@
-const calculation = "9+3*-2";
+const calculation = "9+3*-2+2-7/2+√9";
 
-let newstring = calculation.split(
-  /(?<=[0-9])(\+|\-|\*|\/)(?=[0-9])|(?<=[0-9])(\+|\-|\*|\/)(?=\-[0-9])/
-);
-newstring = newstring.filter((a) => a);
+console.log("Ergebnis", myFunction(calculation));
 
-console.log(newstring);
+function myFunction(calculation) {
+  let newstring = calculation.split(
+    /(?<=[0-9])(\+|\-|\*|\/)(?=[0-9])|(?<=[0-9])(\+|\-|\*|\/)(?=\-[0-9])|(?<=[0-9])(\+|\-|\*|\/)(?=\√[0-9])/
+  );
+  newstring = newstring.filter((a) => a);
 
-const calculationArray = newstring;
+  console.log(newstring);
 
-console.log(calculationArray);
+  const calculationArray = newstring;
 
-const origin = ["√", "*", "+", "/", "-"];
-let unicodeArray = []; // der neue Array, für die CharCodes
-console.log(origin[3].charCodeAt(0).toString());
-for (let i = 0; i < calculationArray.length; i++) {
-  unicodeArray.push(origin[i].charCodeAt(0).toString()); // pushed in den unicodeArray an erster Stelle den Code/Unicode von der Wurzel;
-  // Übersetzt von Dezimal in Unicode, to.String macht String draus;
-}
+  console.log(calculationArray);
 
-{
-  origin[0].normalize(); // falls diese Rechenzeichen vorhanden sind, normalize?
-  console.log(origin); // Anzeigen von origin
-}
-console.log(unicodeArray); // Array zum Überprüfen
-console.log(String.fromCharCode(Number(test))); // Test, ob Nummern raus kommen
-
-while (
-  unicodeArray.includes("8730") || 
-  unicodeArray.includes("42") || 
-  unicodeArray.includes("47") 
-) {
-  let counter;
-  for (let counter = 0; counter < calculationArray.length; counter++) {
-    const element = calculationArray[counter];
-    console.log(element);
-    if (element === "*") {
-      console.log(
-        Number(calculationArray[counter - 1]) *
-          Number(calculationArray[counter + 1])
-      );
-      const ergebnis =
-        Number(calculationArray[counter - 1]) *
-        Number(calculationArray[counter + 1]);
-      calculationArray[counter - 1] = ergebnis;
-      calculationArray[counter + 1] = "x";
-      calculationArray[counter] = "y";
-      calculationArray.splice(calculationArray.indexOf("x"), 1);
-      calculationArray.splice(calculationArray.indexOf("y"), 1);
-      counter = 0;
-      console.log(calculationArray);
-    }
-    if (element === "/") {
-      console.log(
-        Number(calculationArray[counter - 1]) /
-          Number(calculationArray[counter + 1])
-      );
-      const ergebnis =
-        Number(calculationArray[counter - 1]) /
-        Number(calculationArray[counter + 1]);
-      calculationArray[counter - 1] = ergebnis;
-      calculationArray[counter + 1] = "x";
-      calculationArray[counter] = "y";
-      calculationArray.splice(calculationArray.indexOf("x"), 1);
-      calculationArray.splice(calculationArray.indexOf("y"), 1);
-      counter = 0;
-      console.log(calculationArray);
-    }
-    if (element === "√") {
-      console.log(Math.sqrt(Number(calculationArray[counter + 1])));
-      const ergebnis = Math.sqrt(Number(calculationArray[counter + 1]));
-      console.log(ergebnis);
-      calculationArray[counter + 1] = ergebnis;
-      calculationArray[counter] = y;
-      calculationArray.splice(calculationArray.indexOf("y"), 1);
-      counter = 0;
-      console.log(calculationArray);
+  while (
+    calculationArray.includes("√") ||
+    calculationArray.includes("*") ||
+    calculationArray.includes("/")
+  ) {
+    let counter;
+    for (counter = 0; counter < calculationArray.length; counter++) {
+      const element = calculationArray[counter];
+      console.log(element);
+      if (element === "*") {
+        console.log(
+          Number(calculationArray[counter - 1]) *
+            Number(calculationArray[counter + 1])
+        );
+        const ergebnis =
+          Number(calculationArray[counter - 1]) *
+          Number(calculationArray[counter + 1]);
+        calculationArray[counter - 1] = ergebnis;
+        calculationArray.splice(counter, 2);
+        counter = 0;
+        console.log(calculationArray);
+      }
+      if (element === "/") {
+        console.log(
+          Number(calculationArray[counter - 1]) /
+            Number(calculationArray[counter + 1])
+        );
+        const ergebnis =
+          Number(calculationArray[counter - 1]) /
+          Number(calculationArray[counter + 1]);
+        calculationArray[counter - 1] = ergebnis;
+        calculationArray.splice(counter, 2);
+       
+        counter = 0;
+        console.log(calculationArray);
+      }
+      if (element[0] === "√") {
+        console.log("test", element.replace("√", ""));
+        const number = Number(element.replace("√", ""));
+        console.log(calculationArray);
+        calculationArray[counter] = Math.sqrt(number);
+        console.log(calculationArray);
+      }
     }
   }
-}
-while (unicodeArray.includes("43") || unicodeArray.includes("45")) {
-  for (let counter = 0; counter < calculationArray.length; counter++) {
-    const element = calculationArray[counter];
-    console.log(element);
-    if (element === "+") {
-      console.log(
-        Number(calculationArray[counter - 1]) +
-          Number(calculationArray[counter + 1])
-      );
-      const ergebnis =
-        Number(calculationArray[counter - 1]) +
-        Number(calculationArray[counter + 1]);
-      calculationArray[counter - 1] = ergebnis;
-      calculationArray[counter + 1] = "x";
-      calculationArray[counter] = "y";
-      calculationArray.splice(calculationArray.indexOf("x"), 1);
-      calculationArray.splice(calculationArray.indexOf("y"), 1);
-      counter = 0;
-      console.log(calculationArray);
-    }
-    if (element === "-") {
-      console.log(
-        Number(calculationArray[counter - 1]) -
-          Number(calculationArray[counter + 1])
-      );
-      const ergebnis =
-        Number(calculationArray[counter - 1]) -
-        Number(calculationArray[counter + 1]);
-      calculationArray[counter - 1] = ergebnis;
-      calculationArray[counter + 1] = "x";
-      calculationArray[counter] = "y";
-      calculationArray.splice(calculationArray.indexOf("x"), 1);
-      calculationArray.splice(calculationArray.indexOf("y"), 1);
-      counter = 0;
-      console.log(calculationArray);
+  while (calculationArray.includes("+") || calculationArray.includes("-")) {
+    for (let counter = 0; counter < calculationArray.length; counter++) {
+      const element = calculationArray[counter];
+      console.log(element);
+      if (element === "+") {
+        console.log(
+          Number(calculationArray[counter - 1]) +
+            Number(calculationArray[counter + 1])
+        );
+        const ergebnis =
+          Number(calculationArray[counter - 1]) +
+          Number(calculationArray[counter + 1]);
+        calculationArray[counter - 1] = ergebnis;
+        calculationArray.splice(counter, 2)
+        counter = 0;
+        console.log(calculationArray);
+      }
+      if (element === "-") {
+        console.log(
+          Number(calculationArray[counter - 1]) -
+            Number(calculationArray[counter + 1])
+        );
+        const ergebnis =
+          Number(calculationArray[counter - 1]) -
+          Number(calculationArray[counter + 1]);
+        calculationArray[counter - 1] = ergebnis;
+        calculationArray.splice(counter, 2)
+        counter = 0;
+        console.log(calculationArray);
+      }
     }
   }
+  return calculationArray[0]; //notwendig, damit man damit weiter rechnen kann (z.Bsp für Klammern), es wird kein 
+  // Array, in diesem Fall auch nur mit einem Element ausgegeben; z.Bsp [25], sondern eine Zahl (oder in anderen Fällen ein String)
 }
