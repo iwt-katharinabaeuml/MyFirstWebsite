434188af4b0dda146684275acfb6f0bdf33ebb77
Katharina Bäuml <katharina.baeuml@immowelt.de>
2022-09-16 09:57:29 +0200

    Wurzelversuch Stand 1

--- forsite.js
+++ forsite.js
@@ -1,9 +1,8 @@
-const calculation = "L9";
+const calculation = "9+3*-2";
 
 let newstring = calculation.split(
   /(?<=[0-9])(\+|\-|\*|\/)(?=[0-9])|(?<=[0-9])(\+|\-|\*|\/)(?=\-[0-9])/
 );
-
 newstring = newstring.filter((a) => a);
 
 console.log(newstring);
@@ -12,11 +11,26 @@ const calculationArray = newstring;
 
 console.log(calculationArray);
 
-if (calculationArray.includes("L")){
-  console.log("works")
+const origin = ["√", "*", "+", "/", "-"];
+let unicodeArray = []; // der neue Array, für die CharCodes
+console.log(origin[3].charCodeAt(0).toString());
+for (let i = 0; i < calculationArray.length; i++) {
+  unicodeArray.push(origin[i].charCodeAt(0).toString()); // pushed in den unicodeArray an erster Stelle den Code/Unicode von der Wurzel;
+  // Übersetzt von Dezimal in Unicode, to.String macht String draus;
 }
 
-while (calculationArray.includes("/") || calculationArray.includes("*")) {
+{
+  origin[0].normalize(); // falls diese Rechenzeichen vorhanden sind, normalize?
+  console.log(origin); // Anzeigen von origin
+}
+console.log(unicodeArray); // Array zum Überprüfen
+console.log(String.fromCharCode(Number(test))); // Test, ob Nummern raus kommen
+
+while (
+  unicodeArray.includes("8730") || 
+  unicodeArray.includes("42") || 
+  unicodeArray.includes("47") 
+) {
   let counter;
   for (let counter = 0; counter < calculationArray.length; counter++) {
     const element = calculationArray[counter];
@@ -54,20 +68,18 @@ while (calculationArray.includes("/") || calculationArray.includes("*")) {
       console.log(calculationArray);
     }
     if (element === "√") {
-      console.log(Math.sqrt(Number(calculationArray[counter + 1])
-      ))
-      const ergebnis =
-      Math.sqrt(Number(calculationArray[counter + 1]))
-      console.log(ergebnis)
-      calculationArray[counter + 1] = ergebnis
+      console.log(Math.sqrt(Number(calculationArray[counter + 1])));
+      const ergebnis = Math.sqrt(Number(calculationArray[counter + 1]));
+      console.log(ergebnis);
+      calculationArray[counter + 1] = ergebnis;
       calculationArray[counter] = y;
       calculationArray.splice(calculationArray.indexOf("y"), 1);
       counter = 0;
       console.log(calculationArray);
+    }
   }
 }
-}
-while (calculationArray.includes("+") || calculationArray.includes("-")) {
+while (unicodeArray.includes("43") || unicodeArray.includes("45")) {
   for (let counter = 0; counter < calculationArray.length; counter++) {
     const element = calculationArray[counter];
     console.log(element);
